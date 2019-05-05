4c26e905b65d3eea780e0f3e47e692194186131f
BigRedBrent <18352428+BigRedBrent@users.noreply.github.com>
2019-05-05 11:26:44 -0400

    use default download path (#2)
    Merge pull request from BigRedBrent: Use Windows Download Path (from Registry)

--- Win10SpotlightCopy.py
+++ Win10SpotlightCopy.py
@@ -1,17 +1,18 @@
-﻿try:
+try:
 	from PIL import Image #pip install pillow
 except:
 	print('Module not installed, run pip install pillow')
 	input('Enter to exit')
 	exit()
-from os import listdir,environ, sep, makedirs
+from os import listdir, environ, sep, makedirs
 from os.path import isfile
 from shutil import copy2
+import winreg
 
 USERPROFILE = environ['USERPROFILE']
 
 SPOTLIGHTDIR = fr'{USERPROFILE}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'
-DESTDIR = fr'{USERPROFILE}\Downloads\WinSpotlight'
+DESTDIR = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'), '{374DE290-123F-4565-9164-39C4925E467B}')[0] + '\WinSpotlight'
 
 flist = listdir(SPOTLIGHTDIR)
 makedirs(DESTDIR, exist_ok = True)
