f1b7667527d22d13a2c717dd27ab38d396df5c67
BigRedBrent <18352428+BigRedBrent@users.noreply.github.com>
2019-05-05 10:24:00 -0400

    use true default download path
    I have added code to find the actual default download path.
    
    I did not write the script that I have added myself. I simply found it somewhere with a google search.

--- Win10SpotlightCopy.py
+++ Win10SpotlightCopy.py
@@ -1,4 +1,18 @@
-﻿try:
+import os
+
+def get_download_path():
+    """Returns the default downloads path for linux or windows"""
+    if os.name == 'nt':
+        import winreg
+        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+            location = winreg.QueryValueEx(key, downloads_guid)[0]
+        return location
+    else:
+        return os.path.join(os.path.expanduser('~'), 'downloads')
+
+try:
 	from PIL import Image #pip install pillow
 except:
 	print('Module not installed, run pip install pillow')
@@ -11,7 +25,7 @@ from shutil import copy2
 USERPROFILE = environ['USERPROFILE']
 
 SPOTLIGHTDIR = fr'{USERPROFILE}\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets'
-DESTDIR = fr'{USERPROFILE}\Downloads\WinSpotlight'
+DESTDIR = get_download_path() + '\WinSpotlight'
 
 flist = listdir(SPOTLIGHTDIR)
 makedirs(DESTDIR, exist_ok = True)
