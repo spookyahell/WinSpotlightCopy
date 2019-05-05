48b37b6e81be030b300dad8741f00074b32c6d2e
BigRedBrent <18352428+BigRedBrent@users.noreply.github.com>
2019-05-05 10:54:05 -0400

    edit

--- Win10SpotlightCopy.py
+++ Win10SpotlightCopy.py
@@ -1,17 +1,3 @@
-import os
-
-def get_download_path():
-    """Returns the default downloads path for linux or windows"""
-    if os.name == 'nt':
-        import winreg
-        sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
-        downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
-            location = winreg.QueryValueEx(key, downloads_guid)[0]
-        return location
-    else:
-        return os.path.join(os.path.expanduser('~'), 'downloads')
-
 try:
 	from PIL import Image #pip install pillow
 except:
@@ -21,6 +7,14 @@ except:
 from os import listdir,environ, sep, makedirs
 from os.path import isfile
 from shutil import copy2
+
+def get_download_path():
+	import winreg
+	sub_key = r'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders'
+	downloads_guid = '{374DE290-123F-4565-9164-39C4925E467B}'
+	with winreg.OpenKey(winreg.HKEY_CURRENT_USER, sub_key) as key:
+		location = winreg.QueryValueEx(key, downloads_guid)[0]
+	return location
 
 USERPROFILE = environ['USERPROFILE']
 
